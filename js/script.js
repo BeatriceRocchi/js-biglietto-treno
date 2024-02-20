33c19ec21ac23ce465f1a9d2c45a027a41094798
BeatriceRocchi <bea.rocchi93@gmail.com>
2024-02-20 15:38:42 +0100

    [add] style in css and [add] custom messages in js

--- js/script.js
+++ js/script.js
@@ -5,11 +5,18 @@ const discountMinors = 0.2;
 const discountOver = 0.4;
 
 let price = kmUser * priceKm;
+let message = "non è stato applicato alcuno sconto";
 
 if (ageUser <= 18) {
   price += -price * discountMinors;
+  message = `
+  è stato applicato uno sconto del ${discountMinors * 100}%
+  `;
 } else if (ageUser >= 65) {
   price += -price * discountOver;
+  message = `
+  è stato applicato uno sconto del ${discountOver * 100}%
+  `;
 }
 
 price = price.toFixed(2);
@@ -17,6 +24,8 @@ price = price.toFixed(2);
 document.getElementById("output").innerHTML = `
 <p>
 Gentile utente, <br>
-il prezzo richiesto per il tuo biglietto è pari a ${price} €
+il prezzo richiesto per il tuo biglietto è pari a 
+<strong>${price} €</strong>. <br>
+Sulla base dell'età che hai indicato, ${message}.
 </p>
 `;
