77528b428d7bd7746de92cb451aef9713b61a49a
BeatriceRocchi <bea.rocchi93@gmail.com>
2024-02-20 16:13:12 +0100

    [fix] prompt in case of decimal km

--- js/script.js
+++ js/script.js
@@ -1,10 +1,17 @@
-const kmUser = parseInt(prompt("Inserisci i chilometri che devi percorrere: "));
+const kmUser = parseFloat(
+  prompt("Inserisci i chilometri che devi percorrere: ")
+);
 const ageUser = parseInt(prompt("Inserisci la tua età: "));
+
+console.log("Km da percorrere: ", kmUser);
+console.log("Età utente: ", ageUser);
+
 const priceKm = 0.21;
 const discountMinors = 0.2;
 const discountOver = 0.4;
 
 let price = kmUser * priceKm;
+console.log("Prezzo intero: ", price);
 let message = "non è stato applicato alcuno sconto";
 
 if (ageUser <= 18) {
